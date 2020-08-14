40df6d9c266333cf2ce33e1d460d5a667c13fd28
William Herman <williamajherman@gmail.com>
2020-08-14 11:46:34 -0400

    displaying data, needs styling

--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -1,1 +1,12 @@
 // Write your Character component here
+import React from 'react';
+
+export default function Character(props){
+    const {characterInfoObj} = props
+
+    return(
+        <div>
+            <p>Name: {characterInfoObj.name} Height: {characterInfoObj.height} Mass: {characterInfoObj.mass}</p>
+        </div>
+    )
+}
